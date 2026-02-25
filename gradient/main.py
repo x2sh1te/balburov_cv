a1602e460642600a2b1d8138197a8b9829ef4577
nikitbalburov <nikitbalburov1506@gmail.com>
2026-02-25 14:46:34 +0800

    new angle

--- gradient/main.py
+++ gradient/main.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 
 def lerp(v0, v1, t):
     return (1 - t) * v0 + t * v1
-
 size = 100
 image = np.zeros((size, size, 3), dtype="uint8")
-assert image.shape[0] == image.shape[1]
+color1 = [0, 128, 255]
+color2 = [255, 128, 0]
 
-color1 = [255, 128, 0]
-color2 = [0, 128, 255]
-
-for i, v in enumerate(np.linspace(0, 1, image.shape[0])):
-    r = lerp(color1[0], color2[0], v)
-    g = lerp(color1[1], color2[1], v)
-    b = lerp(color1[2], color2[2], v)
-    image[i, :, :] = [r, g, b]
+for i in range(size):
+    for j in range(size):
+        x = j / (size - 1)
+        y = i / (size - 1)
+        t = (x + y) / 2
+        r = lerp(color1[0], color2[0], t)
+        g = lerp(color1[1], color2[1], t)
+        b = lerp(color1[2], color2[2], t)
+        image[i, j] = [r, g, b]
 
 plt.figure(1)
 plt.imshow(image)
